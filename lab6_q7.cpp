c0ce7279c897a53fecbf5700b831344c59f5c583
ShubhamHandles <42141789+ShubhamHandles@users.noreply.github.com>
2018-09-24 21:30:20 +0530

    Update lab6_q7.cpp

--- lab6_q7.cpp
+++ lab6_q7.cpp
@@ -10,28 +10,37 @@ and display the upper case value and vice-versa.
 #include<iostream>
 using namespace std;
 
+//a.Write a user-defined function toUpper() that returns the uppercase of the inputChar received. Use call-by-value.
+
 char toUpper(char ch){
 	return ch-32;
 }
-	
+
+//b.Write a user-defined function toLower() that returns the lowercase of the inputChar received. Use call-by-value.
+
 char toLower(char ch){
 	return ch+32;
 }
 
+//c.Write a main program, that calls the above function based on the input of the user
 
 int main(){
 	
-	char cha;
+	char c;
 
-	cout << "Enter a character to the change case : " << endl;
-	cin >> cha;
+	cout << "Enter a character to change the case : " << endl;
+	cin >> c;
 
-	if(cha>='A' &&  cha<='Z'){
-		cout << toLower(cha);
+	if(c>='A' and  c<='Z'){
+		cout << toLower(c);
 	}
   
-	else if(cha>='a' && cha<='z'){
-		cout << toUpper(cha);
+	else if(c>='a' and c<='z'){
+		cout << toUpper(c);
+	}
+	
+	else {
+		cout << "Your input is not a character!";
 	}
 	cout << endl;
 	
