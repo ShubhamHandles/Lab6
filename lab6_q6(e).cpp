3079cc3ba2a87b99af3ae1edc6b02062114e1ebb
ShubhamHandles <42141789+ShubhamHandles@users.noreply.github.com>
2018-09-24 16:42:07 +0530

    Create lab6_q6(e).cpp

---
+++ lab6_q6(e).cpp
@@ -0,0 +1,13 @@
+/*
+Q.6. Sum of even and odd
+e. Write a main program.  
+i. Declare a variable called sumEven in the main() for the sumEvenNumbers(). 
+sumEvenNumbers() is a value returning function. Use sumEven to hold a returned value. 
+ii.Declare a variable called sumOdd in the main() for the sumOddNumbers(). 
+sumOddNumbers() is a value returning function. Use sumOdd to hold a returned value. 
+iii.Declare a variable called sumSquareEven in the main(), for the sumSqureEvenNumbers(). 
+sumSqureEvenNumbers() is a value returning function. Use sumSquareEven to hold a returned value. 
+iv.Declare a variable called sumSquareOdd in the main(), for the sumSqureOddNumbers(). 
+sumSqureOddNumbers() is a value returning function. Use sumSquareOdd to hold a returned value. 
+v.Then display the values
+*/
